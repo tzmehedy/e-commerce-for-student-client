c55a67fcdf96b6ca05b569d013aab7348592e7a4
unknown <mehedytouhidurzaman@gmail.com>
2024-11-09 23:00:18 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { RouterProvider } from 'react-router-dom'
+import Root from './Layout/Root.jsx'
+import router from './Routes/Router.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}>
+      <Root></Root>
+    </RouterProvider>
+  </StrictMode>,
+)
