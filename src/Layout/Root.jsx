92503676f5ee0cd08225908df2d039d1641d4f19
unknown <mehedytouhidurzaman@gmail.com>
2024-11-09 23:57:00 +0600

    navbar added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavBar from '../Shared/NavBar';
+import Footer from '../Shared/Footer';
 
 const Root = () => {
     return (
-        <div>
-            
-            <Outlet></Outlet>
+      <div>
+        <div className="min-h-[calc(100vh-380px)] md:min-h-[calc(100vh-520px)]">
+          <NavBar></NavBar>
         </div>
+
+        <div className="container md:mx-auto mx-4">
+          <Outlet></Outlet>
+        </div>
+
+        <div>
+          <Footer></Footer>
+        </div>
+      </div>
     );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-         
+         <h1>This is Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, sit soluta. Voluptate et recusandae eligendi? Nesciunt amet hic distinctio! Inventore id sint quidem eveniet odit voluptate optio cupiditate minima sapiente temporibus reprehenderit iusto pariatur, dolorem magni molestiae quasi natus accusamus harum modi ratione consequuntur error non animi! Omnis, atque alias? Unde, numquam! Aliquam praesentium iusto expedita, qui quae esse repellat vero, earum itaque quasi illo eius. Amet omnis in harum, sequi suscipit soluta deserunt alias voluptatibus sapiente deleniti laboriosam molestias architecto corporis minima dolor porro commodi quia nisi! Reiciendis error corporis perferendis quam, hic illum dicta illo deserunt debitis? Cumque!</h1>
         </div>
     );
 };
